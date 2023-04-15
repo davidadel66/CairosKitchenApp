f0aeaf1eaed8a5098bf060c976867ccb5560f296
davidadel66 <davidadel66@gmail.com>
2023-04-15 15:41:00 -0400

    added basic Screens

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://www.cairoskitchen.com/your-logo-url.png' }}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Cairo's Kitchen</Text>
+      <Text style={styles.subtitle}>Authentic Egyptian Cuisine</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('MenuScreen')}
+      >
+        <Text style={styles.buttonText}>View Menu</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#D23A28',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default HomeScreen;
