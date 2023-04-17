86fba2a562972c6234670a5166bbfe58f4ca566a
davidadel66 <davidadel66@gmail.com>
2023-04-16 21:25:32 -0400

    removed emailjs

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import emailjs from 'emailjs-com';
 import axios from 'axios';
 
 const ContactScreen = () => {
